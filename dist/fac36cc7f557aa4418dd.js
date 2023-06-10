b9bb8735c1f0d20372193991edb8dd0242a83276
SAZZAD1Q2 <kurulia115@gmail.com>
2023-06-10 21:35:33 +0600

    html loader updated

--- dist/fac36cc7f557aa4418dd.js
+++
@@ -1,6 +0,0 @@
-import html from "./file.html";
-
-const element = document.createElement('h3');
-element.innerText = "Hello World";
-const text = document.getElementById('text');
-text.appendChild(element);
